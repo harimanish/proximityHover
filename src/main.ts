be6bd974d08607d0fe354c2437a8fa774a273035
Harimanish <harimanishhmp@gmail.com>
2022-09-23 21:20:33 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import './style.css'
+const eyes=document.querySelectorAll<HTMLElement>(".eye");
+const anchor=document.getElementById<HTMLElement>("sourc");
+const rekt=anchor?.getBoundingClientRect();
+const anchorx:any=rekt.left+rekt.width/2;
+const anchory:any=rekt.top+rekt.height/2; 
+document.addEventListener('mousemove',(e)=>{
+  
+  const x=e.clientX;
+  const y=e.clientY;
+
+  
+  const angleDeg=angle(x,y,anchorx,anchory);
+  console.log(angleDeg);
+   
+  eyes.forEach(eye=>{
+    eye.style.transform=`rotate(${angleDeg}deg)`;
+    anchor.style.filter=`hue-rotate(${angleDeg}deg)`;
+})
+})
+
+function angle(cx:number,cy:number,ex:number,ey:number){
+  const dx=ex-cx;
+  const dy=cy-ey;
+  const rad=Math.atan2(dx,dy);
+  const deg=rad*180/Math.PI;
+  return deg;
+}
